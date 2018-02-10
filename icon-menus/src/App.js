5096742f7cfd0a7146f0e8a02792cc34405fc30a
Austin Wright <awguitar86@gmail.com>
2018-02-10 12:05:48 -0700

    imported image

--- icon-menus/src/App.js
+++ icon-menus/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './menu1.css';
+import Menu1 from './BlueShark-Menu-1.svg';
 
 class App extends Component {
   render() {
     return (
       <div class="wrapper">
-        <img src="./BlueShark-Menu-1.svg" alt="menu" class="menu-1"/>
+        <img src={Menu1} alt="menu" class="menu-1"/>
     </div>
     );
   }
