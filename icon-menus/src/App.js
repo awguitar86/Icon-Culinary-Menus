fda119057b7d0e6d8d183a8851baf14e43f1bf03
Austin Wright <awguitar86@gmail.com>
2018-02-10 12:02:30 -0700

    created a react app

---
+++ icon-menus/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './'
+
+class App extends Component {
+  render() {
+    return (
+      <div class="wrapper">
+        <img src="./BlueShark-Menu-1.svg" alt="menu" class="menu-1"/>
+    </div>
+    );
+  }
+}
+
+export default App;
